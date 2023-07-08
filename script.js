86d65583083d115ef58e630d073c213a61a3a606
jolee934 <jolee934@gmail.com>
2023-07-07 23:45:12 -0700

    all

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 const topA = document.querySelector(".top_a");
-const topB = document.querySelector(".top_b");
-const topC = document.querySelector(".top_c");
-const midA = document.querySelector(".mid_a");
-const midB = document.querySelector(".mid_b");
-const midC = document.querySelector(".mid_c");
-const botA = document.querySelector(".bot_a");
-const botB = document.querySelector(".bot_b");
-const botC = document.querySelector(".bot_c");
+// const topB = document.querySelector(".top_b");
+// const topC = document.querySelector(".top_c");
+// const midA = document.querySelector(".mid_a");
+// const midB = document.querySelector(".mid_b");
+// const midC = document.querySelector(".mid_c");
+// const botA = document.querySelector(".bot_a");
+// const botB = document.querySelector(".bot_b");
+// const botC = document.querySelector(".bot_c");
 
-const game = {
-  board: ["", "", "", "", "", "", "", "", ""],
-};
+const cells = document.querySelectorAll(".cell");
 
-var gameBoard = (function () {
-  function publicMethod() {
-    console.log("Tesat");
-  }
+// var gameBoard = (function () {
+//   const game = {
+//     board: ["x", "0", "x", "x", "x", "0", "0", "x", "x"],
+//   };
 
-  return {
-    publicMethod: publicMethod,
-  };
-})();
+//   function updateBoard() {
+//     topA.textContent = game.board[0];
+//     topB.textContent = game.board[1];
+//     topC.textContent = game.board[2];
+//     midA.textContent = game.board[3];
+//     midB.textContent = game.board[4];
+//     midC.textContent = game.board[5];
+//     botA.textContent = game.board[6];
+//     botB.textContent = game.board[7];
+//     botC.textContent = game.board[8];
+//   }
 
-gameBoard.publicMethod();
+//   return {
+//     updateBoard: updateBoard,
+//     game: game,
+//   };
+// })();
 
-//PLAYER 1//
-// const playerOneFactory = score;
+// //PLAYER 1//
+// var playerOneFactory = (function () {
+//   var playerOnescore = 0;
+//   var playerOneTurn = true;
+//   function click() {
+//     if (playerOneTurn === true) {
+//       console.log("p1 working");
+//       playerOneTurn = false;
+//       playerTwoFactory.playerTwoTurn = true;
+//     }
+//   }
 
-topA.textContent = "X";
+//   return {
+//     playerOnescore: playerOnescore,
+//     playerOneTurn: playerOneTurn,
+//     click: click,
+//   };
+// })();
 
+// //PLAYER 2//
+// var playerTwoFactory = (function () {
+//   var playerTwoscore = 0;
+//   var playerTwoTurn = true;
+//   function click() {
+//     if (playerTwoTurn === true) {
+//       console.log("p2 working");
+//       playerTwoTurn = false;
+//       playerOneFactory.playerOneTurn = true;
+//     }
+//   }
+//   return {
+//     playerTwoscore: playerTwoscore,
+//     playerTwoTurn: playerTwoTurn,
+//   };
+// })();
 
-function playerClick() {
-  if 
-}
+console.log(cells);
