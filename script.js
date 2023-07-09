6207a5d8233a41597bddcd98ab913612690f7081
jolee934 <jolee934@gmail.com>
2023-07-08 17:43:59 -0700

    all

--- script.js
+++ script.js
@@ -7,59 +7,119 @@ const midC = document.querySelector(".mid_c");
 const botA = document.querySelector(".bot_a");
 const botB = document.querySelector(".bot_b");
 const botC = document.querySelector(".bot_c");
-
+const restartButton = document.querySelector(".restart");
+const backButton = document.querySelector(".back");
+const difficultyButton = document.querySelector(".difficulty");
 const cells = document.querySelectorAll(".cell");
 
+//*********Game Board Module *************/
 var gameBoard = (function () {
   const game = {
-    board: ["x", "0", "x", "x", "x", "0", "0", "x", "x"],
+    board: ["", "", "", "", "", "", "", "", ""],
   };
+
+  var gameRunning = true;
+
+  function restartGame() {
+    game.board.forEach((n, i, arr) => {
+      arr[i] = "";
+    });
+    updateBoard();
+  }
 
   function updateBoard() {
     cells.forEach((cell) => {
       const i = cell.dataset.index;
       cell.textContent = game.board[i];
     });
+    if (
+      game.board[0] != "" &&
+      game.board[1] != "" &&
+      game.board[2] != "" &&
+      game.board[0] === game.board[1] &&
+      game.board[0] === game.board[2] &&
+      gameRunning === true
+    ) {
+      if (pOne.turn === true) {
+        console.log("Player 1 wins");
+      } else {
+        console.log("Player 2 wins");
+      }
+      gameRunning = false;
+    }
   }
 
   return {
     updateBoard: updateBoard,
+    restartGame: restartGame,
+    gameRunning: gameRunning,
     game: game,
   };
 })();
 
-gameBoard.updateBoard();
+//*********Player Factory*************/
+const playerFactory = (score, turn) => {
+  return { score, turn };
+};
 
-function click() {
-  console.log("clicked");
-}
+const pOne = playerFactory("0", true);
+const pTwo = playerFactory("0");
+console.log(pOne);
+console.log(pTwo);
+
+// var pOne = (function () {
+//   let pOneTurn = true;
+//   let pOneScore = 0;
+
+//   return {
+//     pOneTurn: pOneTurn,
+//     pOneScore: pOneScore,
+//   };
+// })();
+
+// var pTwo = (function () {
+//   let pTwoScore = 0;
+
+//   return {
+//     pTwoScore: pTwoScore,
+//   };
+// })();
+
+//*********Clicking action *************/
 
 cells.forEach((cell) => {
   cell.addEventListener("click", function (e) {
     const clickedCell = e.target;
-    if (clickedCell.classList.contains("top_a")) {
-      gameBoard.game.board[0] = "A";
+    const i = cell.dataset.index;
+    if (gameBoard.game.board[i] === "" && pOne.turn === true) {
+      gameBoard.game.board[i] = "X";
       gameBoard.updateBoard();
-    }
-    if (clickedCell.classList.contains("top_b")) {
-      gameBoard.game.board[1] = "B";
-      gameBoard.updateBoard();
-    }
-    if (clickedCell.classList.contains("top_c")) {
-      gameBoard.game.board[2] = "C";
-      gameBoard.updateBoard();
-    }
-    if (clickedCell.classList.contains("mid_a")) {
-      gameBoard.game.board[3] = "D";
-      gameBoard.updateBoard();
-    }
-    if (clickedCell.classList.contains("mid_b")) {
-      gameBoard.game.board[4] = "E";
-      gameBoard.updateBoard();
-    }
-    if (clickedCell.classList.contains("mid_c")) {
-      gameBoard.game.board[5] = "F";
-      gameBoard.updateBoard();
+      pOne.turn = false;
     }
   });
 });
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", function (e) {
+    const clickedCell = e.target;
+    const i = cell.dataset.index;
+    if (gameBoard.game.board[i] === "" && pOne.turn === false) {
+      gameBoard.game.board[i] = "0";
+      gameBoard.updateBoard();
+      pOne.turn = true;
+    }
+  });
+});
+
+//*********Restart game *************/
+restartButton.addEventListener("click", gameBoard.restartGame);
+
+//***********Winning mechanic *************/
+
+// 123, 345, 789;
+
+// 147;
+// 258;
+// 369;
+// 159;
+// 357;
